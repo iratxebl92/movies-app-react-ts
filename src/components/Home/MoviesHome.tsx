b5b0f2e9110c5710bf481727b72bf80f003298ed
Iratxe <iratxe92@gmail.com>
2024-12-20 16:38:23 +0100

    Arreglos estilos

--- src/components/Home/MoviesHome.tsx
+++ src/components/Home/MoviesHome.tsx
@@ -1,17 +1,21 @@
-import { contentTypes, timePeriods } from "../../utils/constants"
-import { SwitchTab } from "../SwitchTab"
-import { TopRated } from "./TopRated"
-import { Trending } from "./Trending"
-
-
+import { contentTypes, timePeriods } from "../../utils/constants";
+import { SwitchTab } from "../SwitchTab";
+import { TopRated } from "./TopRated";
+import { Trending } from "./Trending";
 
 export const MoviesHome = () => {
   return (
-    <div className="mt-10 max-w-75">
-      <SwitchTab options={contentTypes} />
-        <Trending/>
-      <SwitchTab options={timePeriods} />
-        <TopRated/>
+    <div className="mt-10 max-w-7xl mx-auto">
+      <div className="flex justify-between">
+        <span className="text-2xl ml-3 font-bold">Trending</span>
+        <SwitchTab options={contentTypes} />
+      </div>
+      <Trending />
+      <div className="flex justify-between">
+        <span className="text-2xl ml-3 font-bold">Top Rated</span>
+        <SwitchTab options={timePeriods} />
+      </div>
+      <TopRated />
     </div>
-  )
-}
+  );
+};
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,5 @@ export const BASE_URL = "https://api.themoviedb.org/3";
 export const contentTypes = ['Movies', 'Tv Show']
 
 export const timePeriods = ['Week', 'Day']
+
+//TODO: Hacer que sea dinamico el idioma
