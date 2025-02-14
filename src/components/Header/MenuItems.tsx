613ba3e71cbeadb91c3bd6831b4652fb11279e86
Iratxe <iratxe92@gmail.com>
2025-02-14 09:25:33 +0100

    Arreglo icono idiomas

--- src/components/Header/MenuItems.tsx
+++ src/components/Header/MenuItems.tsx
@@ -5,19 +5,18 @@ import { useTranslation } from "react-i18next"; // Importar el hook de i18n
 
 export const MenuItems = () => {
   const { theme, toggleTheme, language, setLanguage } = useMoviesStore();
-  const { t } = useTranslation();  // Usamos el hook de i18n para acceder a las traducciones
-
+  const { t } = useTranslation(); 
   return (
     <nav>
       <ul className="md:flex md:flex-row md:space-x-4 md:mr-1">
         <li className="md:pr-5 md:hover:underline md:hover:decoration-3 md:hover:decoration-details hover:cursor-pointer ">
-          {t('home')} {/* Traducción para "Inicio" */}
+          {t('home')} 
         </li>
         <li className="md:pr-5 md:hover:underline md:hover:decoration-3 md:hover:decoration-details hover:cursor-pointer ">
-          {t('movies')} {/* Traducción para "Películas" */}
+          {t('movies')} 
         </li>
         <li className="md:pr-5 md:hover:underline md:hover:decoration-3 md:hover:decoration-details hover:cursor-pointer ">
-          {t('tv')} {/* Traducción para "Series" */}
+          {t('tv')} 
         </li>
         <li className="dark:text-white md:pr-5 md:hover:underline hover:decoration-3 md:hover:decoration-details">
           <button onClick={toggleTheme}>
@@ -28,18 +27,14 @@ export const MenuItems = () => {
             )}
           </button>
         </li>
-        <li className="md:pr-5 md:pt-1.5 md:hover:underline md:hover:decoration-3 md:hover:decoration-details">
-          <div className="md:absolute flex text-center justify-center">
-            <div>
+        <li className="md:pr-5 md:hover:underline md:hover:decoration-3 md:hover:decoration-details">
               <select
                 value={language}
                 onChange={(e) => setLanguage(e.target.value)}
               >
-                <option value="en">{t('english')}</option> {/* Traducción para "English" */}
-                <option value="es">{t('spanish')}</option> {/* Traducción para "Español" */}
+                <option value="en">EN</option> 
+                <option value="es">ES</option>
               </select>
-            </div>
-          </div>
         </li>
       </ul>
     </nav>
