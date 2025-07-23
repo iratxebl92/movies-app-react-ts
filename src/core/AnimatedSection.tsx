5b58761dc029a1b77e98da1dcf797655b28903f9
Iratxe <iratxe92@gmail.com>
2025-07-23 14:17:03 +0200

    Fix: Eliminar console.log

--- src/core/AnimatedSection.tsx
+++ src/core/AnimatedSection.tsx
@@ -24,3 +24,4 @@ export const AnimatedSection = ({ children }: { children: React.ReactNode }) => 
     );
   };
 
+  
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -24,5 +24,5 @@ export const AppRouter = () => {
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  ); 
 };
